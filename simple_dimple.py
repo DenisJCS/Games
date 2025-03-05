43d2b056f4eaa484384a9e4982cb3ed3808a5bef
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2025-03-05 11:02:05 -0500

    Update simple_dimple.py

--- simple_dimple.py
+++ simple_dimple.py
@@ -7,3 +7,30 @@ for day in range(3):
 
 print("Leaves:", leaves)
 display_simulation()
+
+
+
+leaves = 3
+light = 1
+water = 1
+
+for day in range(3):
+  leaves += light
+  leaves += water
+  if leaves > 8:
+    leaves += 2
+    print("Growth spurt!")
+
+print("Leaves:", leaves)
+display_simulation()
+
+
+# Output
+Growth spurt!
+Leaves: 11
+       🌿🌿🌿
+     🌿🌿🌿🌿🌿
+      🌿🌿🌿⟋
+       (^_^)
+        | |
+        | |
